b745349d9cef00b8bf4ca75e3a1ef8b08bdd7a4c
Ji-Hoon Ha <122500323+JHHa223@users.noreply.github.com>
2023-06-17 14:30:43 +0900

    Update optflow_tvl1.py

--- optical_flow/optflow_tvl1.py
+++ optical_flow/optflow_tvl1.py
@@ -24,6 +24,9 @@ for i in range(len(path_list)):
     path = glob.glob(base_path+path_list[i]+'/*.npy')
     path = sorted(path)
     print(path)
+    
+#### Single-Temporal Model ####
+    
     for j in range(0,18):
         # Reading input data
         img0 = np.load(path[j]) # input image at t-10
@@ -37,7 +40,33 @@ for i in range(len(path_list)):
         h,w,_ = flow_1.shape
         flow_1[:,:,0] += np.arange(w)
         flow_1[:,:,1] += np.arange(h)[:,np.newaxis]
-        prevImg = cv2.remap(img1, flow_1, None, cv2.INTER_CUBIC)
+        Img_f = cv2.remap(img1, flow_1, None, cv2.INTER_CUBIC)
         
         #saving future frame at t+10
-        np.save(new_path+'o_precipitation_202007301000+'+str(10*(j+1))+'min.npy',prevImg)
+        np.save(new_path+'o_precipitation_202007301000+'+str(10*(j+1))+'min.npy',Img_f)
+
+#### Multi-Temporal Model ####
+    
+    for j in range(0,18):
+        # Reading input data
+        img0 = np.load(path[j]) # input image at t-30
+        img1 = np.load(path[j+1]) # input image at t-20
+        img2 = np.load(path[j+2]) # input image at t-10
+        img3 = np.load(path[j+3]) # input image at t
+
+        flow_10 = dtvl1.calc(img1, img0, None)*1
+        flow_20 = dtvl1.calc(img2, img1, None)*(1/2)
+        flow_30 = dtvl1.calc(img3, img2, None)*(1/3)
+
+        flow_tot = (flow_10+flow_20+flow_30)/3
+        
+        # saving optical flow field
+        np.save(new_path+'o_vector_Multi_202007301000+'+str((j+1)*10)+'min.npy',flow_tot)
+
+        h,w,_ = flow_tot.shape
+        flow_tot[:,:,0] += np.arange(w)
+        flow_tot[:,:,1] += np.arange(h)[:,np.newaxis]
+        Img_Multi_f = cv2.remap(img1, flow_tot, None, cv2.INTER_CUBIC)
+        
+        #saving future frame at t+10
+        np.save(new_path+'o_precipitation_Multi_202007301000+'+str(10*(j+1))+'min.npy',Img_Multi_f)
