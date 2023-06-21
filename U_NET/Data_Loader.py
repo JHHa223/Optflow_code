b18ad8a4ed96f423899069bb98f70e39e24ea8e0
Ji-Hoon Ha <122500323+JHHa223@users.noreply.github.com>
2023-06-21 14:53:40 +0900

    Update Data_Loader.py

--- U_NET/Data_Loader.py
+++ U_NET/Data_Loader.py
@@ -8,12 +8,18 @@ import torchvision
 from torchvision import transforms
 import glob
 
+# data structure: np.stack(img0,img1,img_pred,img_gt), size=(nx,ny,4)
+# img0 -> radar image at t-10 with size=(nx,ny)
+# img1 -> radar image at t with size=(nx,ny)
+# img_pred -> predicted image at t+10 through optical flow with size=(nx,ny)
+# img_gt -> ground truth at t+10 with size=(nx,ny)
+
 class Loader(Dataset):
 
     def __init__(self, dummy=0, path='', transform=None):
         self.dummy = dummy
         if dummy == 0:
-            self.path = glob.glob('/data0/jhha223/OptFlow/srf/train_set_tvl1/*.npy')
+            self.path = glob.glob('/data0/jhha223/OptFlow/srf/train_set_tvl1/*.npy') 
         else:
             self.path = glob.glob('/data0/jhha223/OptFlow/srf/val_set_tvl1/*.npy')
 
