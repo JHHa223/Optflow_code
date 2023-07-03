785845d506cd734c526a24bef7ccd70839480455
Ji-Hoon Ha <122500323+JHHa223@users.noreply.github.com>
2023-07-03 09:54:03 +0900

    Update Data_Loader.py

--- U_NET/Data_Loader.py
+++ U_NET/Data_Loader.py
@@ -23,25 +23,13 @@ class Loader(Dataset):
         else:
             self.path = glob.glob('/data0/jhha223/OptFlow/srf/val_set_tvl1/*.npy')
 
-        self.transform = transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.CenterCrop(size=(400,400)),
-            transforms.RandomCrop(size=(256,256)),
-            transforms.RandomRotation(90),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5),
-            transforms.ToTensor()
-            ])
-
     def __len__(self):
         return len(self.path)
 
     def __getitem__(self, idx):
 
         img = np.load(self.path[idx])
-        img = img.astype(np.uint8)
-        img = self.transform(img)
-        img = img.cpu().detach().numpy().reshape(4,256,256)
+        img = img.reshape(4,256,256) # for example, nx=256, ny=256
 
         return img
 
